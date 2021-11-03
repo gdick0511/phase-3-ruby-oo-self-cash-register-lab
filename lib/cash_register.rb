b10f36260a7d30cfd1d50ce0dff053ee00ef5cc7
garrettdick <garrettd0511@gmail.com>
2021-11-03 17:54:51 -0500

    commit tree fcb0910157718e3f25d44bbbc18723819033511d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
 
+    attr_accessor :total, :discount, 
+
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+    end
+
+    # def discount
+    #     @discount
+    # end
+
+    # def total
+    #     @total
+    # end
+
+    def add_item(title, price, quantity=0)
+        @quantity = quantity
+        @total = @total + price * quantity
+    end
+    
+end
